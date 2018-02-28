0d53fc3eedbdee3fe4ac3fb7d389d6ffe77c6aa8
jaredwilli <jaredwilli@gmail.com>
2018-02-28 15:35:09 -0500

    add StarIcon component

---
+++ src/components/StarIcon/StarIcon.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/** Star Icon component for star ratings */
+const StarIcon = ({ fillType = 'empty' }) => {
+    if (fillType === 'full') {
+        return (
+            <svg width="32" height="32" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"/></svg>
+        )
+    } else if (fillType === 'half') {
+        return (
+            <svg width="32" height="32" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1250 957l257-250-356-52-66-10-30-60-159-322v963l59 31 318 168-60-355-12-66zm452-262l-363 354 86 500q5 33-6 51.5t-34 18.5q-17 0-40-12l-449-236-449 236q-23 12-40 12-23 0-34-18.5t-6-51.5l86-500-364-354q-32-32-23-59.5t54-34.5l502-73 225-455q20-41 49-41 28 0 49 41l225 455 502 73q45 7 54 34.5t-24 59.5z"/></svg>
+        )
+    }
+
+    return (
+        <svg width="32" height="32" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1201 1004l306-297-422-62-189-382-189 382-422 62 306 297-73 421 378-199 377 199zm527-357q0 22-26 48l-363 354 86 500q1 7 1 20 0 50-41 50-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"/></svg>
+    )
+};
+
+StarIcon.propTypes = {
+    /** The fill */
+    fillType: PropTypes.string
+};
+
+export default StarIcon;
---
+++ src/docs/examples/StarIcon/Example.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import StarIcon from 'react-reuse/StarIcon';
+
+/** Cool stuff dude */
+function Example() {
+    return (
+        <div>
+            <StarIcon />
+            <StarIcon fillType="half" />
+            <StarIcon fillType="full" />
+        </div>
+    );
+};
+
+export default Example;

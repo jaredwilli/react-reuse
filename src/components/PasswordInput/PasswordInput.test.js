8fa966d999eb2241dfe628867d230592093f609f
Jared Williams <jaredwilli@gmail.com>
2018-03-15 19:41:43 -0400

    add travis file and do simulation test for password input component

--- src/components/PasswordInput/PasswordInput.test.js
+++ src/components/PasswordInput/PasswordInput.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
+import * as setupTests from '../../setupTests';
+
 import PasswordInput from './PasswordInput';
 
+
 describe('PasswordInput', () => {
+    it('toggles the input type when show/hide password is clicked', () => {
+        const wrapper = shallow(<PasswordInput
+            htmlId="test"
+            name="test"
+            value=""
+            onChange={() => {}}
+            showVisibilityToggle
+            />
+        );
+
+        // PasswordInput should have a type of password initially
+        expect(wrapper.find({ type: 'password' })).toHaveLength(1);
+        expect(wrapper.find({ type: 'text' })).toHaveLength(0);
+
+        wrapper.find('a').simulate('click');
+        expect(wrapper.find({ type: 'password' })).toHaveLength(0);
+        expect(wrapper.find({ type: 'text' })).toHaveLength(1);
+    });
+
     it('hides password quality by default', () => {
         const tree = renderer.create(<PasswordInput
             htmlId="test"
